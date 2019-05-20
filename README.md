f0207bf28ff72ea3b158fbf93cd3f9fcc89d1f5e
kasselTrankos <alvaro.touzon@gmail.com>
2019-05-20 23:50:00 +0200

    add proxy working in remote

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-## The soket rigoe's leg
+## The soket riego's leg
 
 This is the socket server from port 300 that comunicate directly with arduino and mobile.
+
+Info:
+ - [create systemctl riegoserver.service for run node](https://nodesource.com/blog/running-your-node-js-app-with-systemd-part-1/)
--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const app = require('express')();
-const http = require('http').createServer(app);
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
 });
 
 http.listen(3000, function(){
