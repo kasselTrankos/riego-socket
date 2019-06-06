2e60fb3ad89965674b6a6f71180d1bad2ac503d0
kasselTrankos <alvaro.touzon@gmail.com>
2019-06-06 22:20:38 +0200

    made order list

--- riegos.js
+++ riegos.js
@@ -1,11 +1,11 @@
 const MongoClient = require('mongodb').MongoClient;
 const ObjectID = require('mongodb').ObjectID;
 const {url, dbName, riegos, config} = require('./config.js');
+const getClient = async (url) =>  await MongoClient.connect(url);
 
 const getConfig = async () => {
-  let client;
+  const client = await getClient(url);
   try {
-    client = await MongoClient.connect(url);
     const [result] =  await client.db(dbName).collection(config).find({_id: ObjectID('5cf970b0c52511b14d75b351')}).toArray(); 
     return result;
   } catch(error) {
@@ -16,14 +16,10 @@ const getConfig = async () => {
   } 
 }
 const putConfig = async (id, duration) => {
-  let client;
-  
+  const client = await getClient(url);
   try {
-    client = await MongoClient.connect(url);
-    console.log(id, 'dur', duration, 'pppp');
-    return  await client.db(dbName).collection(config).updateOne({_id: ObjectID(id)}, {$set: {duration: duration}}, {upsert: true}); 
+    return  await client.db(dbName).collection(config).updateOne({_id: ObjectID(id)}, {$set: {duration: Number(duration)}}, {upsert: true}); 
   } catch(error) {
-    console.log(error);
     return {duration: 30}; 
     // throw error;
   } finally {
@@ -31,9 +27,8 @@ const putConfig = async (id, duration) => {
   } 
 }
 const madeRiego = async (riego) => {
-  let client;
+  const client = await getClient(url);
   try {
-    client = await MongoClient.connect(url);
     const {duration} = await getConfig();
     return await client.db(dbName).collection(riegos).insertOne({riego: riego, date: new Date(), duration, inmediate: true, programated: false}); 
   } catch(error) {
@@ -43,22 +38,18 @@ const madeRiego = async (riego) => {
   } 
 };
 const findAll = async () => {
-  let client;
+  const client = await getClient(url);
   try {
-    client = await MongoClient.connect(url);
-    return  await client.db(dbName).collection(riegos).find({}).toArray();
+    return  await client.db(dbName).collection(riegos).find({}).sort( { date: -1 } ).toArray();
   } catch (err) {
     return  {};
-    //soon control it
-    // throw err;
   } finally {
     client.close(); 
   }
 };
 const riegoDone = async (id) => {
-  let client;
+  const client = await getClient(url);
   try {
-    client = await MongoClient.connect(url);
     return  await client.db(dbName).collection(riegos).updateOne({_id: ObjectID(id)}, {$set: {done: new Date(), isDone: true}}, {upsert: true});
   } catch (err) {
     return  {id, error: true};
