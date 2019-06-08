72f58dd07e2b8d704c413db89c050213a8d854a2
kasselTrankos <alvaro.touzon@gmail.com>
2019-06-08 11:56:55 +0200

    kalendar tune for get seconds

--- kalendar.js
+++ kalendar.js
@@ -1,7 +1,7 @@
 const moment = require('moment');
 const fs = require('fs');
 
-const madeKalendar = ({start, end, hour, minute}) => {
+const madeKalendar = ({start, end, hour, minute, duration}) => {
   const days = Math.abs(moment(start).diff(moment(end), 'days'));
   const dates = Array.from({length: days}, (_, i)=> {
     return  moment(start).add(i, 'days').set({hour, minute});
@@ -13,7 +13,7 @@ const madeKalendar = ({start, end, hour, minute}) => {
       },
       sheduler: "* * * * * *"
     };
-    fs.writeFileSync('kalendar.json', JSON.stringify(Object.assign({}, json, {dates}), 'utf8', {mode: 0755});
+    fs.writeFileSync('kalendar.json', JSON.stringify(Object.assign({}, json, {dates, duration})), 'utf8', {mode: 0755});
     return {message: 'update kalendar', status: true};
   } catch(err) {
     return {message: 'ko kalendar', status: false}
