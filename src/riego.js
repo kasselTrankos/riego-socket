47878b45663f66eccca4c613aa87d31729c4328b
kasselTrankos <alvaro.touzon@gmail.com>
2019-06-17 15:50:28 +0200

    made concat loose in recorded

--- src/riego.js
+++ src/riego.js
@@ -1,19 +1,27 @@
 const {tagged} = require('daggy');
-const {empty, of, concat} = require('fantasy-land');
+const {empty, of, concat, equals} = require('fantasy-land');
 
 const Riego = tagged('Riego', ['date', 'duration', 'hour', 'minute', 'active']);
 Riego[of] = value => Riego(value);
-Riego[empty] = () => Riego({active: false});
+Riego[empty] = () => Riego(new Date('1900-01-01'), 0, 0, 0, false);
+Riego.prototype[equals] = function(that) {
+  return [+new Date(this.date) === +new Date(that.date),
+    this.duration === that.duration,
+    this.hour === that.hour, this.minute === that.minute].every(Boolean);
+};
 Riego.prototype[concat] = function(that) {
-  const formatZero = value => value <= 9 ? `0${value}` : value;
+  console.log('--------------------------------');
+  console.log('0WTF', this, 'emty ', that) ;
   /// here is shouting chain !!!! please become a chain man!!!
   const thatDate = new Date(new Date(new Date(that.date).setHours(that.hour)).setMinutes(that.minute));
   const thisDate = new Date(new Date(new Date(this.date).setHours(this.hour)).setMinutes(this.minute));
   const validDate = thatDate >= thisDate ? thatDate : thisDate;
-  const date = `${validDate.getFullYear()}-${formatZero(validDate.getMonth() + 1)}-${formatZero(validDate.getDate())}`;
-  const duration = that.duration + this.duration;
+  const date = thatDate >= thisDate ? that.date : this.date;
+  const duration = +that.duration + +this.duration;
+  
   const hour = validDate.getHours();
   const minute = validDate.getMinutes();
-  return Riego(date, duration, hour, minute, true);
+  const active = this.active || that.active;
+  return Riego(date, duration, hour, minute, active);
 }
 module.exports = Riego;
--- test/riego.spec.js
+++ test/riego.spec.js
@@ -1,20 +1,40 @@
 const Riego = require ('./../src/Riego');
-const {concat} = require('fantasy-land');
+const {concat} = require('ramda');
+const FL = require('fantasy-land');
 const laws = require('fantasy-laws');
 const jsc = require ('jsverify');
 const show = require ('sanctuary-show');
 const Z = require ('sanctuary-type-classes');
 
-const RiegoArb = jsc.json.smap(Riego, riego => riego.value, show);
+const allwaysTrue = jsc.bool.generator.map(bool => true)
 const arbRiego = jsc.record({
   date: jsc.datetime,
-  hour: jsc.nat(24),
-  minute: jsc.nat(60),
-  duration: jsc.nat(60),
+  hour: jsc.integer(0, 24),
+  minute: jsc.integer(0, 60),
+  duration: jsc.integer(0, 60),
   active: jsc.bool,
 });
 const {associativity} = laws.Semigroup(Z.equals, Riego);
+const {leftIdentity, rightIdentity} = laws.Monoid((concated, _this) => {console.log('soy concated', concated, 'hay this?', _this); return true}, Riego);
 const testAssociativity = associativity (arbRiego, arbRiego, arbRiego);
+const testRightIdentity = rightIdentity (arbRiego);
+const testLeftIdentity = leftIdentity (arbRiego);
+
+console.log(FL);
 describe('RiegoArb => ',  () => {
  it('testAssociativity', testAssociativity);
+ it('custom ', ()=> {
+    const A = Riego[FL.empty]();
+    const B = Riego(new Date(), 12, 10, 11, true);
+    const C = B[FL.concat](A);
+    const F = A[FL.concat](B);
+    const D = concat(A, B);
+    c = curry2 (Z.concat);
+    const G = c(A)(B);
+    console.log(A[FL.concat], FL.concat);
+    // console.log('000', Z.equals(G, B), G, B);
+    // console.log(A, 'B:', B, 'C: ', C);
+ }) 
+ it('testRightIdentity', testRightIdentity);
+//  it('testLeftIdentity', testLeftIdentity);
 });
