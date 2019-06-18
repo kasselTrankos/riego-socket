8bf268b64cbfd7e7a9a606b8c193c224813b0822
kasselTrankos <alvaro.touzon@gmail.com>
2019-06-18 19:25:35 +0200

    made identity for  law functor because using Riego as data then can works

--- src/riegos.js
+++ src/riegos.js
@@ -1,19 +1,29 @@
 const {taggedSum} = require('daggy');
-const {empty, map, of} = require('fantasy-land');
+const {equals, empty, map, of} = require('fantasy-land');
 const Riego  = require('./riego');
 const Riegos = taggedSum('Riegos', {
   Cons: ['head', 'tail'],
   Nil: [],
 });
-// Riegos[of] = value => Riegos(value);
+Riegos[of] = value => Riegos.from(value);
 Riegos.from = function (xs) {
   return xs.reduceRight(
     (acc, x) => Riegos.Cons(x, acc),
     Riegos.Nil
   )
 }
-
-Riegos.prototype.map = function (f) {
+Riegos.prototype[equals] = Riegos.prototype.equals = function (that) {
+  const isEqual = _this => _that => _this[equal](_that);
+  const _this = this.toArray();
+  const _that = that.toArray();
+  const _equals = _that.map(el => _this.some(_el => _el[equals](el)));
+  const areEquals =  _equals.reduce((acc, val)=> { 
+    if(!val) acc = false; 
+    return val;}, 
+    true);
+  return areEquals;
+}
+Riegos.prototype[map] = Riegos.prototype.map = function (f) {
   return this.cata({
     Cons: (head, tail) => Riegos.Cons(
       f(head), tail.map(f)
@@ -24,9 +34,7 @@ Riegos.prototype.map = function (f) {
 Riegos.prototype.filter = function (f) {
   return this.cata({
     Cons: (head, tail) => {
-      const _head = f(head) ? head : Riegos.Nil
-      return _head === Riegos.Nil ?
-        tail.filter(f) : Riegos.Cons(_head, tail.filter(f))
+      return !f(head) ? tail.filter(f) : Riegos.Cons(head, tail.filter(f))
     },
     Nil: () => Riegos.Nil
   })
--- test/riegos.spec.js
+++ test/riegos.spec.js
@@ -1,9 +1,34 @@
 const Riegos = require ('./../src/riegos');
+const Riego = require ('./../src/riego');
 const {concat} = require('ramda');
 const FL = require('fantasy-land');
 const laws = require('fantasy-laws');
 const jsc = require ('jsverify');
 const show = require ('sanctuary-show');
 const Z = require ('sanctuary-type-classes');
-const R = Riegos.from([1, 2, 3, 9 ,90]).map(x => x * 2).toArray();
-console.log(Riegos.from([13,3,3, 90, 23, 3,3,45,6,2,3]).filter(x=> x % 3 === 0 ).toArray());
+
+const arbRiego = jsc.record({
+  date: jsc.datetime(new Date('2015-11-10'), new Date()),
+  hour: jsc.integer(0, 24),
+  minute: jsc.integer(0, 60),
+  duration: jsc.integer(0, 60),
+  active: jsc.bool,
+});
+const blessRiegos = (amount = 4) => jsc.bless({
+  generator: ()=> {
+    const riegos = Array.from({length: amount}, ()=> {
+      const riego = arbRiego.generator()
+      return Riego(riego.date, riego.duration, riego.hour, riego.minute, true);
+    }); 
+    return Riegos.from(riegos);
+  }
+});
+const {identity, composition} = laws.Functor(x=> true, Riegos);
+const testIdentity = identity(blessRiegos());
+
+const testComposition = composition(blessRiegos(), blessRiegos(), blessRiegos());
+describe('Riegos  => ',  () => {
+  // it('testComposition', testComposition);
+  it('testIdentity', testIdentity);
+
+});
