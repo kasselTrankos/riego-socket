bd8e965ed6bb1a5e04f4c23fc12e9f0d3cb818ac
kasselTrankos <alvaro.touzon@gmail.com>
2019-06-21 17:17:56 +0200

    removed files

--- test/irrigation.spec.js
+++ test/irrigation.spec.js
@@ -16,7 +16,7 @@ const blessCons = (length=3) => jsc.bless({
 });
 const {identity, composition} = laws.Functor(Z.equals, Irrigation);
 const testSomeIdentity = identity(blessSome);
-const testConsIdentity = identity(blessCons(5));
+const testConsIdentity = identity(blessCons(190));
 const testConsComposition = composition(blessCons(4), jsc.bless({generator:() =>  x => {x.a =  x.a * 3; return x}}), jsc.bless({generator: ()=> x => {x.b = x.b +10; return x}}));
 const testSomeComposition = composition(blessCons(4), jsc.bless({generator:() =>  x => {x.duration =  x.duration * 3; return x}}), jsc.bless({generator: ()=> x => {x.y = x.y +10; return x}}));
 
