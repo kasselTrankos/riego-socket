d388b4d6aba6635a544f1272cac7cbf2c62ef861
kasselTrankos <alvaro.touzon@gmail.com>
2019-06-24 12:57:55 +0200

    add benchmark

--- benchmark/index.js
+++ benchmark/index.js
@@ -1,9 +1,8 @@
-const Irrigation = require ('./../src/irrigation');
+const Irrigation = require ('../src/irrigation');
+var Benchmark = require('benchmark');
 const laws = require('fantasy-laws');
 const jsc = require ('jsverify');
 const Z = require ('sanctuary-type-classes');
-
-
 
 const blessSome = (length=3) => jsc.bless({
   generator: ()=> {
@@ -19,17 +18,17 @@ const blessCons = (length=3) => jsc.bless({
     return Irrigation.from(elms);
   }
 });
+
 const {identity, composition} = laws.Functor(Z.equals, Irrigation);
-const testSomeIdentity = identity(blessSome());
-const testConsIdentity = identity(blessCons(9));
-const testConsComposition = composition(blessCons(4), jsc.bless({generator:() =>  x => {x.a =  x.a * 3; return x}}), jsc.bless({generator: ()=> x => {x.b = x.b +10; return x}}));
 const testSomeComposition = composition(blessSome(4), jsc.bless({generator:() =>  x => {x.duration =  x.duration * 3; return x}}), jsc.bless({generator: ()=> x => {x.y = x.y +10; return x}}));
 
-
-describe('Irrigation => ',  () => {
-  it('testSomeIdentity', testSomeIdentity);
-  it('testConsIdentity', testConsIdentity);
-  it('testConsComposition', testConsComposition);
-  it('testSomeComposition', testSomeComposition);
-});
-
+const suite = new Benchmark.Suite;
+suite.add('testSomeComposition#test', testSomeComposition)
+.on('cycle', function(event) {
+  console.log(String(event.target));
+})
+.on('complete', function() {
+  console.log('Fastest is ' + this.filter('fastest').map('name'));
+})
+// run async
+.run({ 'async': true });
