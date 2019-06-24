28cc984fcfc6544994f4809663905bcf8e7c728c
kasselTrankos <alvaro.touzon@gmail.com>
2019-06-24 14:18:05 +0200

    seems more effective usin head, tail in hard stress

--- benchmark/index.js
+++ benchmark/index.js
@@ -12,23 +12,20 @@ const blessSome = (length=3) => jsc.bless({
     return Irrigation.Some(elms);
   }
 });
-const blessSomeEs6 = (length=3) => jsc.bless({
+const blessCons = (length=3) => jsc.bless({
   generator: ()=> {
-    const elms = Array.from({length}, ()=> 
-      ({duration: jsc.integer(0, 60).generator(), y: jsc.integer(0, 60).generator()}));
-    return Irrigation.SomeEs6(elms);
+    const elms = Array.from({length}, ()=> ({a: jsc.integer(0, 60).generator(), b: jsc.integer(0, 60).generator()}));
+    return Irrigation.from(elms);
   }
 });
 
 const {identity, composition} = laws.Functor(Z.equals, Irrigation);
-const testSomeIdentity = identity(blessSome(604));
-const testConsIdentityEs6 = identity(blessSomeEs6(604));
+const testSomeIdentity = identity(blessSome(1400));
+const testConsIdentityCons= identity(blessCons(1400));
 
-const testSomeComposition = composition(blessSome(3004), jsc.bless({generator:() =>  x => {x.duration =  x.duration * 3; return x}}), jsc.bless({generator: ()=> x => {x.y = x.y +10; return x}}));
-const testSomeCompositionEs6 = composition(blessSomeEs6(3004), jsc.bless({generator:() =>  x => {x.duration =  x.duration * 3; return x}}), jsc.bless({generator: ()=> x => {x.y = x.y +10; return x}}));
 const suite = new Benchmark.Suite;
 suite.add('testSomeIdentity#test', testSomeIdentity)
-suite.add('testConsIdentityEs6#test', testConsIdentityEs6)
+suite.add('testConsIdentityCons#test', testConsIdentityCons)
 .on('cycle', function(event) {
   console.log(String(event.target));
 })
