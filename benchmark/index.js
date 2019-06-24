0c887bf270b266e68203e9dd5d455118d3949e6d
kasselTrankos <alvaro.touzon@gmail.com>
2019-06-24 13:14:37 +0200

    yes we are abusing of map, so functor map i think is only a interface

--- benchmark/index.js
+++ benchmark/index.js
@@ -12,18 +12,21 @@ const blessSome = (length=3) => jsc.bless({
     return Irrigation.Some(elms);
   }
 });
-const blessCons = (length=3) => jsc.bless({
+const blessSomeEs6 = (length=3) => jsc.bless({
   generator: ()=> {
-    const elms = Array.from({length}, ()=> ({a: jsc.integer(0, 60).generator(), b: jsc.integer(0, 60).generator()}));
-    return Irrigation.from(elms);
+    const elms = Array.from({length}, ()=> 
+      ({duration: jsc.integer(0, 60).generator(), y: jsc.integer(0, 60).generator()}));
+    return Irrigation.SomeEs6(elms);
   }
 });
 
 const {identity, composition} = laws.Functor(Z.equals, Irrigation);
 const testSomeComposition = composition(blessSome(4), jsc.bless({generator:() =>  x => {x.duration =  x.duration * 3; return x}}), jsc.bless({generator: ()=> x => {x.y = x.y +10; return x}}));
+const testSomeCompositionEs6 = composition(blessSomeEs6(4), jsc.bless({generator:() =>  x => {x.duration =  x.duration * 3; return x}}), jsc.bless({generator: ()=> x => {x.y = x.y +10; return x}}));
 
 const suite = new Benchmark.Suite;
 suite.add('testSomeComposition#test', testSomeComposition)
+suite.add('testSomeCompositionEs6#test', testSomeCompositionEs6)
 .on('cycle', function(event) {
   console.log(String(event.target));
 })
--- src/irrigation.js
+++ src/irrigation.js
@@ -3,6 +3,7 @@ const {map, equals, of} = require('fantasy-land');
 
 const Irrigation = taggedSum('Irrigation', {
   Some: ['items'],
+  SomeEs6: ['items'],
   None: [],
   Cons: ['head', 'tail'],
   Nil: []
@@ -34,14 +35,20 @@ Irrigation.prototype[equals] = Irrigation.prototype.equals = function (that) {
         }
         return _exists;
       },
-      // items_.reduce((acc, {duration: duration_, y:y_}) => {
-      //   const find = items.filter(({duration, y}) => duration_ === duration && y === y_);
-      //   if(!find.length) {
-      //     acc = false;
-      //   }
-      //   return acc;
-      // }, true),
-
+      SomeEs6: () => false,
+      Nil: () => false,
+      None: () => false,
+      Cons: () => false,
+    }),
+    SomeEs6: (items) => that.cata({
+      SomeEs6:(items_) => items_.reduce((acc, {duration: duration_, y:y_}) => {
+      const find = items.filter(({duration, y}) => duration_ === duration && y === y_);
+      if(!find.length) {
+        acc = false;
+      }
+      return acc;
+      }, true),
+      Some: () => false,
       Nil: () => false,
       None: () => false,
       Cons: () => false
@@ -51,7 +58,9 @@ Irrigation.prototype[equals] = Irrigation.prototype.equals = function (that) {
                                                   : head.a === head_.a && head.b === head_.b,
       Nil: () => false,
       None :() => false,
-      Some: () => false
+      Some: () => false,
+      SomeEs6: () => false,
+
     }),
     Nil: () => true,
     None: () => true,
@@ -60,6 +69,7 @@ Irrigation.prototype[equals] = Irrigation.prototype.equals = function (that) {
 Irrigation.prototype[map] = Irrigation.prototype.map = function (f) {
   return this.cata({
     Some: (items) => Irrigation.Some(items.map(item => f(Object.assign({}, item)) )),
+    SomeEs6: (items) => Irrigation.Some(items.map(item => f(Object.assign({}, item)) )),
     None: () => this,
     Nil: () => this,
     Cons: (head, tail) => Irrigation.Cons(
@@ -74,6 +84,7 @@ Irrigation.prototype.toArray = function () {
       x, ... acc.toArray()
     ],
     Some: (duration, y ) => [duration, y],
+    SomeEs6: (duration, y ) => [duration, y],
     None: () => [],
     Nil: () => [],
   })
