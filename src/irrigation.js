4a47cb45049c009286c3247c980e0589ec76ea59
kasselTrankos <alvaro.touzon@gmail.com>
2019-06-26 22:17:00 +0200

    add orderarion fantasy land Ord

--- src/irrigation.js
+++ src/irrigation.js
@@ -1,7 +1,6 @@
 const {taggedSum} = require('daggy');
-const {map, equals, of} = require('fantasy-land');
+const {map, equals, lte} = require('fantasy-land');
 const Irrigation = taggedSum('Irrigation', {
-  Some: ['items'],
   Cons: ['head', 'tail'],
   Nil: []
 });
@@ -11,32 +10,15 @@ Irrigation.from = function (xs) {
     Irrigation.Nil
   )
 }
+Irrigation.prototype[lte] = Irrigation.prototype.lte = function (that) {
+  return this.head.a <= that.a;
+}
+
 Irrigation.prototype[equals] = Irrigation.prototype.equals = function (that) {
   return this.cata({
-    Some: (items) => that.cata({
-      Some: (items_) => {
-        let _exists = false;
-        for (var i = 0; i < items.length; i ++){
-          const {duration, y} = items[i];
-          _exists = false;
-          for (var t = 0; t < items_.length; t ++) {
-            const {duration: duration_, y:y_} = items_[t];
-            if(duration === duration_ && y === y_){
-              _exists = true;
-              break;
-            }
-          }
-          if(!_exists) break; 
-        }
-        return _exists;
-      },
-      Cons: () => false,
-      Nil: () => false,
-    }),
     Cons: (head, tail) => that.cata({
       Cons: (head_, tail_) => head.a === head_.a && head.b === head_.b ? tail.equals(tail_)
                                                   : head.a === head_.a && head.b === head_.b,
-      Some: () => false,
       Nil: () => false,
     }),
     Nil: () => true,
@@ -46,26 +28,23 @@ Irrigation.prototype.swap = function () {
   return this.cata({
     Cons: (head, tail) => {
       const {head_, tail_} =  tail.cata({
-        Some: (items) => false,
         Cons: (head_, tail_) => {
           return {head_, tail_};
         },
         Nil: () => this,
       });
-      if(!tail.is && head_.a < head.a) {
+      if(!tail.is && tail.lte(head)) {
         tail = tail_;
         return Irrigation.Cons(head_, Irrigation.Cons(head, tail.swap()))
       }
       return Irrigation.Cons(head, tail.swap());
     },
-    Some: (items) => this,
     Nil:() => this,
   });
 }
 let cons;
 Irrigation.prototype.sort = function () {
   return this.cata({
-    Some: (items) => false,
     Cons: (head, tail) => {
       if(!cons) cons = this
       cons = cons.swap();
@@ -77,7 +56,6 @@ Irrigation.prototype.sort = function () {
 }
 Irrigation.prototype[map] = Irrigation.prototype.map = function (f) {
   return this.cata({
-    Some: (items) => Irrigation.Some(items.map(item => f(Object.assign({}, item)) )),
     Cons: (head, tail) => Irrigation.Cons(
       f(Object.assign({}, head)), tail.map(f)
     ),
@@ -90,7 +68,6 @@ Irrigation.prototype.toArray = function () {
     Cons: (x, acc) => [
       x, ... acc.toArray()
     ],
-    Some: (duration, y ) => [duration, y],
     Nil: () => [],
   })
 }
--- test/irrigation.spec.js
+++ test/irrigation.spec.js
@@ -5,13 +5,6 @@ const Z = require ('sanctuary-type-classes');
 
 
 
-const blessSome = (length=3) => jsc.bless({
-  generator: ()=> {
-    const elms = Array.from({length}, ()=> 
-      ({duration: jsc.integer(0, 60).generator(), y: jsc.integer(0, 60).generator()}));
-    return Irrigation.Some(elms);
-  }
-});
 const blessCons = (length=3) => jsc.bless({
   generator: ()=> {
     const elms = Array.from({length}, ()=> ({a: jsc.integer(0, 60).generator(), b: jsc.integer(0, 60).generator()}));
@@ -19,19 +12,19 @@ const blessCons = (length=3) => jsc.bless({
   }
 });
 const {identity, composition} = laws.Functor(Z.equals, Irrigation);
-const testSomeIdentity = identity(blessSome());
 const testConsIdentity = identity(blessCons(9));
 
 const testConsComposition = composition(blessCons(4), jsc.bless({generator:() =>  x => {x.a =  x.a * 3; return x}}), jsc.bless({generator: ()=> x => {x.b = x.b +10; return x}}));
-const testSomeComposition = composition(blessSome(4), jsc.bless({generator:() =>  x => {x.duration =  x.duration * 3; return x}}), jsc.bless({generator: ()=> x => {x.y = x.y +10; return x}}));
+const ordTestTransitivity = laws.Ord.transitivity(blessCons(), blessCons(), blessCons());
+
 
 const A = Irrigation.from([ {a:180, b: 12}, {a: 4, b:0}, {a:11, b:2}, {a:134, b:109}, {a:190, b: 112},{a: 212, b:1}, {a: 1, b:12}, {a: 1901, b:2}]);
 const B = A.sort();
-// console.log(B.toArray())
+console.log(B.toArray())
 describe('Irrigation => ',  () => {
-  it('testSomeIdentity', testSomeIdentity);
   it('testConsIdentity', testConsIdentity);
   it('testConsComposition', testConsComposition);
-  it('testSomeComposition', testSomeComposition);
+  it('ordTestTransitivity', ordTestTransitivity);
+
 });
 
