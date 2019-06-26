af287d623dc8c628301985dfe72f013ceee5ea1b
kasselTrankos <alvaro.touzon@gmail.com>
2019-06-26 23:18:59 +0200

    order bug

--- test/irrigation.spec.js
+++ test/irrigation.spec.js
@@ -7,7 +7,7 @@ const Z = require ('sanctuary-type-classes');
 
 const blessCons = (length=3) => jsc.bless({
   generator: ()=> {
-    const elms = Array.from({length}, ()=> ({a: jsc.integer(0, 60).generator(), b: jsc.integer(0, 60).generator()}));
+    const elms = Array.from({length}, ()=> ({a: jsc.integer(0, 460).generator(), b: jsc.integer(0, 60).generator()}));
     return Irrigation.from(elms);
   }
 });
@@ -16,13 +16,32 @@ const testConsIdentity = identity(blessCons(9));
 const {associativity} = laws.Semigroup(Z.equals, Irrigation);
 
 const testConsComposition = composition(blessCons(4), jsc.bless({generator:() =>  x => {x.a =  x.a * 3; return x}}), jsc.bless({generator: ()=> x => {x.b = x.b +10; return x}}));
-const ordTestTransitivity = laws.Ord.transitivity(blessCons(), blessCons(), blessCons());
+const ordTestTransitivity = laws.Ord.transitivity(blessCons(900), blessCons(900), blessCons(900));
 const testAssociativity = associativity (blessCons(4), blessCons(4), blessCons(4));
 
 const A = Irrigation.from([ {a:180, b: 12}, {a: 4, b:0}, {a:11, b:2}, {a:134, b:109}, {a:190, b: 112},{a: 212, b:1}, {a: 1, b:12}, {a: 1901, b:2}]);
 const B = Irrigation.from([ {a:120, b: 12}, {a: 554, b:0}]);
 // const C = A.sort();
+<<<<<<< Updated upstream
 // console.log(B.toArray())
+=======
+const now = new Date();
+
+const G = blessCons(400).generator();
+const M = G.sort().toArray();
+const S = G.toArray();
+for(let u = 0; u< S.length; u++){
+  console.log('ornb', S[u], u);
+}
+for(let i =0 ; i< M.length; i++){
+  console.log(M[i], M[i +1], i, M.length);  
+  if(i +1 < M.length && M[i].a> M[i+1].a) {
+    console.log('bith bug', M[i], M[i +1])
+    break;
+  }
+}
+// console.log(now, new Date(), G.toArray())
+>>>>>>> Stashed changes
 // console.log('0000909090',A.concat(B).toArray());
 describe('Irrigation => ',  () => {
   it('testConsIdentity', testConsIdentity);
