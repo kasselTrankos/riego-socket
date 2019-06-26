329b006e4cc3b3d6975843810bbf9724d7c3ff77
kasselTrankos <alvaro.touzon@gmail.com>
2019-06-26 14:19:51 +0200

    if use rest , not correct

--- src/irrigation.js
+++ src/irrigation.js
@@ -48,21 +48,22 @@ let cons;
 Irrigation.prototype.swap = function(head_, tail_) {
   return tail_.cata({
     Some: (items) => false,
-    Cons: (head, tail) => {
+    Cons: (_head, rest) => {
       // console.log(head_.a, head.a, head_.a > head.a, '0000000\n');
-      if(head_.a > head.a){
+      if(head_.a > _head.a){
         // head.mierda = {a1: head_.a, a2: head.a};
 
         // console.log('0', head, 'p', head_);
-        const tmp = head;
-        head = head_;
+        const tmp = _head;
+        _head = head_;
         head_ = tmp;
-        head.moved = a;
+        _head.moved = a;
         head_.moved = a;
         // console.log('1' ,head, 'p', head_);
 
       }
-      return {head, head_};
+      // console.log(_head, '::::::', head_);
+      return {left: _head, right: head_, rest};
     },
     Nil: () => this,
   });
@@ -70,9 +71,17 @@ Irrigation.prototype.swap = function(head_, tail_) {
 Irrigation.prototype.sorting = function () {
   return this.cata({
     Cons: (head, tail) => {
-      console.log('sorting, head', head, 'cons');
-      this.swap(head, tail);
-      tail.sorting()
+      // console.log('sorting, head', head, 'cons');
+      const {left, right, rest} = this.swap(head, tail);
+      // tail.sorting()
+      // if(!tail.is) {
+        console.log('left;', left, 'right', right);
+        return Irrigation.Cons(right, Irrigation.Cons(left, tail.sorting()));
+        console.log(gg.toArray, tail.is);
+
+      // }
+      // return Irrigation.Cons(head_, tail.sorting());
+      // tail.sorting();
     },
     Some: (items) => this,
     Nil:() => this,
@@ -85,8 +94,9 @@ Irrigation.prototype.sort = function () {
   return this.cata({
     Some: (items) => false,
     Cons: (head, tail) => {
-      console.log('sort is: ', head,' this');
-      cons.sorting();
+      // console.log('sort is: ', head,' this');
+      const s = cons.sorting();
+      console.log('ppppppppppppp',s.toArray())
       ++a;
       return Irrigation.Cons(head, tail.sort());
     },
