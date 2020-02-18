93d6acd78b99fa4628575862723c384273c54554
kasselTrankos <alvaro.touzon@gmail.com>
2020-02-18 23:32:10 +0100

    feat: - work fine with mine version

--- fp/rx/subscription.js
+++
@@ -1,7 +0,0 @@
-function Subscription(unsubscribe) {
-  this.unsubscribe = unsubscribe;
-}
-Subscription.create = function(unsubscribe){
-  return new Subscription(unsubscribe)
-}
-module.exports = Subscription;
