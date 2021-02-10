cd6f2aa7615809c27a524da3875e62a489283c92
kasselTrankos <alvaro.touzon@gmail.com>
2021-02-11 00:53:49 +0100

    feat: exit jest

--- app.js
+++ app.js
@@ -34,8 +34,8 @@ app.get('/', async (req, res) => {
 });
 
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
+// http.listen(3000, function(){
+//   console.log('listening on *:3000');
+// });
 
 module.exports = { app }
