8c139a87306cf293e37211380843b96ceba188a6
kasselTrankos <alvaro.touzon@gmail.com>
2021-04-13 23:02:32 +0200

    feat: add login

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 // get from https://github.com/socketio/socket.io/issues/1015#issuecomment-16161612
 require("tls").SLAB_BUFFER_SIZE = 100 * 1024; // 100Kb
 const app = require('express')();
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser')
+const session = require('express-session')
+const R = require('ramda')
 
 const { initializeConfig } = require('./app/config')
 const { initializeKalendar } = require('./app/kalendar')
 
+var auth = function(req, res, next) {
+  if (req.session.auth)
+    return next();
+  else
+    return res.sendStatus(401);
+};
+
 ////////////////////////////////////////////////////////////////////////////
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 app.use(bodyParser.json()); // support json encoded bodies
-
+app.use(session({
+  secret: 'njdsflsdfjldf-asdfkudslñl-2C44-4D44-WppQ38S',
+  resave: true,
+  saveUninitialized: true
+}))
 
 const startApp = (io) => {
   // init config routes
@@ -17,7 +30,25 @@ const startApp = (io) => {
   initializeKalendar(io, app)
   app.get('/', async (req, res) => {
     res.sendFile(__dirname + '/public/login.html');
-  });
+  })
+  app.get('/home', auth, async (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+  })
+  app.post('/login', async (req, res) => {
+    const auth = R.converge((user, pwd) => user === 'yo' && pwd === 'yo', [
+      R.pipe(
+        R.prop('body'),
+        R.prop('user')
+      ),
+      R.pipe(
+        R.prop('body'),
+        R.prop('pwd')
+      )
+    ])
+    req.session.auth = auth(req)
+    res.redirect('/home');
+  })
+
 
 }
 
